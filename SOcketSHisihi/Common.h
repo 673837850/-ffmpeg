8b41ada3a5a35c89879ed6ae2996ab7ac96f466d
吕超 <673837850@qq.com>
2017-11-20 10:29:05 +0800

    first commit

---
+++ SOcketSHisihi/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  SOcketSHisihi
+//
+//  Created by 吕超 on 2017/11/16.
+//  Copyright © 2017年 MAXTV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+@end
---
+++ SOcketSHisihi/Common.m
@@ -0,0 +1,13 @@
+//
+//  Common.m
+//  SOcketSHisihi
+//
+//  Created by 吕超 on 2017/11/16.
+//  Copyright © 2017年 MAXTV. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+@end
